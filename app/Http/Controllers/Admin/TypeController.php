91c8948995503fd7f9764ce609f503f9a95e4fc4
marcodedemo <marco.dedemo@gmail.com>
2023-05-24 14:38:11 +0200

    feat: added projects preview on types shows

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -70,7 +71,9 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
-        return view('admin/types/show', compact('type'));
+        $projects = Project::all();
+
+        return view('admin/types/show', compact('type', 'projects'));
         
     }
 
--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -10,10 +10,91 @@
     <span><strong>Types: </strong>{{count($type->projects)}}</span>
     <hr>
     <span>{{$type->description}}</span>
+    <hr>
+
+    <h3>Projects</h3>
+    <div id="projects-container" class="container d-flex flex-wrap gap-3 mt-4">
+        @foreach ($type->projects as $project)
+    
+        
+            <div class="card" style="width: 18rem;">
+                <div class="card-body d-flex flex-column justify-content-between">
+    
+                    <div class="card-content">
+    
+                        <h5 class="card-title">{{$project->title}}</h5>
+                        
+                        <h6 class="card-subtitle text-secondary fst-italic py-1">{{$project->type->name ?? ''}}</h6>
+                        
+                        <div class="card-text">
+                            <ul class="d-flex flex-column gap-1">
+    
+                                <li><strong>Technologies: </strong>
+    
+                                    <div class="logo-container d-flex gap-2 py-1">
+                                        @if (str_contains(strtolower($project->technologies), "html"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/html.png')}}" alt="html5 logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "css"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/css.png')}}" alt="css3 logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "js"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/js.png')}}" alt="js logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "php"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/php.png')}}" alt="php logo"></div>
+                                        @endif
+    
+                                        @if (str_contains(strtolower($project->technologies), "vue"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/vue.png')}}" alt="Vuejs logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "vite"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/vite.png')}}" alt="Vite logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "bootstrap"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/bootstrap.png')}}" alt="bootstrap logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "mysql"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/sql.png')}}" alt="MySQL logo"></div>
+                                        @endif
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "laravel"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/laravel.png')}}" alt="Laravel logo"></div>
+                                        @endif
+                                    </div>
+                                
+                                </li>
+                            
+                                <li><strong>Execution Date: </strong> {{date("d/m/Y", strtotime($project['execution_date']))}}</li>
+                                <li><strong>Description: </strong> {{$project['description']}}</li>
+                                
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div class="link-container">
+    
+                        <a href="{{$project->link}}" class="card-link align-self-start justify-self-end">Github</a>
+    
+                    </div>
+                </div>
+            </div>
+    
+        @endforeach
+    </div>
+
+    
 
 
 
-    <div id="buttons" class="d-flex flex-column gap-3">
+    <div id="buttons" class="d-flex flex-column gap-3 py-5">
 
         
         <div id="type-edit">
