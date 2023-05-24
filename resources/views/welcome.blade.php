ec60580c73eeb2b7cd5be75df5f558c7a0fa8110
marcodedemo <marco.dedemo@gmail.com>
2023-05-24 17:51:43 +0200

    fix: guest dashboard project view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,82 +11,84 @@
 </div>
 
 <div class="content">
-    <div class="container d-flex flex-wrap gap-3">
+    <div class="container d-flex flex-wrap gap-3 mt-5">
 
         @foreach ($projects as $project)
-
+    
+        
+        
             <div class="card" style="width: 18rem;">
                 <div class="card-body d-flex flex-column justify-content-between">
-                    <h5 class="card-title">{{$project->title}}</h5>
+    
+                    <div class="card-content">
+    
+                        <h5 class="card-title">{{$project->title}}</h5>
+                        
+                        <h6 class="card-subtitle text-secondary fst-italic py-1">{{$project->type->name ?? ''}}</h6>
+                        
                         <div class="card-text">
-                            <ul>
-                                <li>
-                                    <strong>Languages: </strong>
-        
-                                    <div class="logo-container d-flex gap-2">
-        
-                                        
-                                        @if (str_contains(strtolower($project['language']), "html"))
+                            <ul class="d-flex flex-column gap-1">
+    
+                                <li><strong>Technologies: </strong>
+    
+                                    <div class="logo-container d-flex gap-2 py-1">
+                                        @if (str_contains(strtolower($project->technologies), "html"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/html.png')}}" alt="html5 logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['language']), "css"))
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "css"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/css.png')}}" alt="css3 logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['language']), "js"))
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "js"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/js.png')}}" alt="js logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['language']), "php"))
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "php"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/php.png')}}" alt="php logo"></div>
                                         @endif
-        
-                                        
-                                        
-                                        
-                                    </div>
-                                </li>
-
-                                <li><strong>Frameworks: </strong>
-
-                                    <div class="logo-container d-flex gap-2">
-        
-                                        @if (str_contains(strtolower($project['framework']), "vue"))
+    
+                                        @if (str_contains(strtolower($project->technologies), "vue"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/vue.png')}}" alt="Vuejs logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['framework']), "vite"))
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "vite"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/vite.png')}}" alt="Vite logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['framework']), "bootstrap"))
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "bootstrap"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/bootstrap.png')}}" alt="bootstrap logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['framework']), "mysql"))
-                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/sql.png')}}" alt="MySQL logo"></div>
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "mysql"))
+                                        <div class="logo"><img src="{{Vite::asset('resources/img/logos/mysql.png')}}" alt="MySQL logo"></div>
                                         @endif
-        
-                                        @if (str_contains(strtolower($project['framework']), "laravel"))
+                                        
+                                        @if (str_contains(strtolower($project->technologies), "laravel"))
                                         <div class="logo"><img src="{{Vite::asset('resources/img/logos/laravel.png')}}" alt="Laravel logo"></div>
                                         @endif
-        
                                     </div>
                                 
                                 </li>
-
-                                <li><strong>Execution Date: </strong> {{$project->execution_date}}</li>
-                                <li><strong>Description: </strong> {{$project->description}}</li>
-
+                            
+                                <li><strong>Execution Date: </strong> {{date("d/m/Y", strtotime($project['execution_date']))}}</li>
+                                <li><strong>Description: </strong> {{$project['description']}}</li>
+                                
                             </ul>
                         </div>
-                    <a href="{{$project->link}}" class="card-link">Link Github</a>
+                    </div>
+                    <div class="link-container">
+    
+                        <a href="{{$project->link}}" class="card-link align-self-start justify-self-end">Github</a>
+    
+                    </div>
                 </div>
-          </div>
+            </div>
+    
+    
             
         @endforeach
-
+    
     </div>
 </div>
 @endsection
